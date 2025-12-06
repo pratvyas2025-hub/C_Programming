e126a045b3a10093b369e49479d8e39190aad7c3
pratvyas2025-hub <pratvyas2025@gmail.com>
2025-12-06 19:22:08 +0530

    Allocated memory using DMA for int

--- Pointer/pointer_main.c
+++ Pointer/pointer_main.c
@@ -1,3 +1,14 @@
 //Simple pointer program 
 
 #include "pointer_main.h"
+
+int main()
+{
+  int *p = NULL;
+  p = (int *)malloc(sizeof(int));
+
+  if (p == NULL)
+  printf("Memory not allocated");
+  else
+  printf("Memory allocated");
+}
